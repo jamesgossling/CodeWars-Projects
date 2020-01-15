350d87312a8ad886bc78dee0079e805bf8ef8db4
James Gossling <jamesgossling@users.noreply.github.com>
2020-01-15 13:02:01 -0500

    Create JGossling-SQL_math_basics_demographics.sql

---
+++ SQL/7kyu/JGossling-SQL_math_basics_demographics.sql
@@ -0,0 +1,17 @@
+/*
+7 kyu - SQL Basics: Maths with String Manipulations
+https://www.codewars.com/kata/594901ba44645fd7bd00005f
+-------------------------------------------------------
+Given a demographics table in the following format:
+
+** demographics table schema **
+
+id
+name
+birthday
+race
+return a single column named 'calculation' where the value is the bit length of name, added to the number of characters in race.
+*/
+
+SELECT (BIT_LENGTH(d.name) + LENGTH(d.race)) as calculation 
+FROM demographics d;
